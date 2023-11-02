826430f91f2d49c600671de0a8a99bdbc1828245
FarhathH <farhathh0@gmail.com>
2023-11-02 10:48:56 +0000

    please commit

---
+++ Day 2/main.py
@@ -0,0 +1,72 @@
+#17th Oct 2023
+#identifyer = value
+my_num = 6
+
+#Outputs sentences
+print("Hello World...")
+
+#Attempted to write my name
+print("... I am Farhath :)")
+print("Farhath Hussain :D")
+
+#divide
+print(900 / 9)
+
+#modulo (remainder)
+print(20 % 3)
+
+#multiplication
+print(7 * 7)
+
+#subtraction
+print(700 - 65)
+
+#Printing out variable
+print(my_num)
+
+#first num variable
+my_num_1 = 5
+
+#second num variable
+my_num_2 = 8
+
+#Multipled both num variables together
+print("The answer for 5 x 8 is:", my_num_1 * my_num_2)
+
+#Multipled both num variables together again
+print("I repeat! The answer is", my_num_1 * my_num_2)
+
+#Asked for user input
+my_input = input("What's your favourite chocolate?:")
+print(my_input,". Interesting choice")
+
+#Asking for first name
+first_name = input("What's your name, stranger?:")
+
+
+#Asking for last name using the f string
+last_name = input(f"Hi {first_name}. What's your last name?:")
+print("Nice to finally meet you,",first_name+" "+last_name)
+
+#Asking for favourite colour
+fav_colour = input(f"So {first_name}, do you have a favourite colour?:")
+print(fav_colour, ". That's my favourite too")
+
+#Outputed a string in the format:
+#The book store has x number of books still in stock
+total_books = 300
+books_sold = 120
+result = total_books - books_sold
+print(f"The book store has {result} number of books in stock")
+
+#18th Oct 2023
+#Kickoff
+#Three variables to describe name, hometown and favourite thing
+#Output as: my name is x, I grew up in y and my favourite thing is z
+name = "Farhath"
+homeTown = "Bristol"
+favouriteThing = "Oero Bites"
+print(f"My name is {name}. I grew up in {homeTown} and my favourite thing is {favouriteThing} :D")
+#test
+
+
